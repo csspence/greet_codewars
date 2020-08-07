0e48208b7b4410f3a45299882af4b54ee879d0f3
Crew Spence <csspence123@gmail.com>
2020-08-07 05:47:01 -0500

    Wrote working solution.

--- greet_codewars.js
+++ greet_codewars.js
@@ -2,6 +2,6 @@
 Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
 */
 
-function greet(name){
-  //your code here
+const greet = (name) => {
+  return "Hello, " + name + " how are you doing today?";
 }
